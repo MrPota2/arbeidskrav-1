1360afc5f72bfbcabdad5c8e7e6f57752ba947df
DotWallop (Mats) <79384776+DotWallop@users.noreply.github.com>
2025-09-25 21:46:24 +0200

    Extracted error handling from files and added into error_utils as helper

--- oppgaver/oppgave-5/helpers/error_utils.py
+++ oppgaver/oppgave-5/helpers/error_utils.py
@@ -5,3 +5,24 @@ def print_error_summary(error_row_count: int) -> None:
 
     if error_row_count > 0:
         print(("=" * 30) + "\n") # Adds a seperator line between warnings and output
+
+
+def is_row_columns_valid(row: dict, row_num: int, headers, verbose: bool=False) -> bool:
+    """Function returns False if row columns are not valid, and True if there are no errors"""
+
+    # Empty field sanitizer
+    if any(row[col] is None or row[col] == "" for col in headers):  # Worked with GPT for this line, to find a comparator that would match if ANY column returned None or blank TODO: add this comment in readme instead
+        if verbose:
+            print(f"WARNING: Row {row_num} skipped. Empty column detected.")
+        return False
+
+    # Value checker for integer-only columns
+    try:
+        int(row["Forlenget"])
+        int(row["Låneperiode"])
+    except ValueError as e:
+        if verbose:
+            print(f"WARNING: Row {row_num} skipped. Non-int value in int column: ({e})")
+        return False
+
+    return True
--- oppgaver/oppgave-5/oppgave-5_2.py
+++ oppgaver/oppgave-5/oppgave-5_2.py
@@ -1,6 +1,6 @@
 import csv
 
-from helpers.error_utils import print_error_summary
+from helpers.error_utils import print_error_summary, is_row_columns_valid
 
 def books_per_genre(verbose: bool=False) -> dict:
     """
@@ -18,23 +18,8 @@ def books_per_genre(verbose: bool=False) -> dict:
         headers = csv_reader.fieldnames
 
         for row_num, row in enumerate(csv_reader, start=1):
-
-            # Empty field sanitizer
-            if any(row[col] is None or row[col] == "" for col in headers): # Worked with GPT for this line, to find a comparator that would match if ANY column returned None or blank
+            if not is_row_columns_valid(row, row_num, headers, verbose):
                 num_of_error_rows += 1
-                if verbose:
-                    print(f"WARNING: Row {row_num} skipped. Empty column detected.")
-                continue
-
-            # Value checker for integer-only columns
-            try:
-                int(row["Forlenget"])
-                int(row["Låneperiode"])
-
-            except ValueError as e:
-                num_of_error_rows += 1
-                if verbose:
-                    print(f"WARNING: Row {row_num} skipped. Non-int value in int column: ({e})")
                 continue
 
             # Counter logic
